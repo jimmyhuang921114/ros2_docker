e9a3582abfec1c970fa7ed3ef5999caf10ce2a31
jimmy <jimmyjimmyhuang1114@gmail.com>
2025-10-12 20:32:41 +0800

    ros2_desktop_version

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,30 @@
-FROM ros:humble-ros-base 
+# ==== ROS 2 Humble Desktop（含 RViz2 / rqt 等）====
+FROM osrf/ros:humble-desktop
 
+#environment setting
+ENV DEBIAN_FRONTEND=noninteractive \
+    TZ=Asia/Taipei \
+    SHELL=/bin/bash
+#install base tools
 RUN apt-get update && apt-get install -y --no-install-recommends \
-      build-essential python3-colcon-common-extensions git && \
-    rm -rf /var/lib/apt/lists/*
+      build-essential \
+      python3-colcon-common-extensions \
+      tree \
+      git \
+      sudo \
+      locales \
+    && rm -rf /var/lib/apt/lists/*
 
+RUN locale-gen en_US.UTF-8 && update-locale LANG=en_US.UTF-8
+ENV LANG=en_US.UTF-8
+ENV LC_ALL=en_US.UTF-8
+
+#add user
 RUN useradd -ms /bin/bash work && \
     echo "work ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 USER work
 
-# COPY ./src ./src
-# RUN . /opt/ros/$ROS_DISTRO/setup.sh && colcon build --symlink-install
-
-
+#set work dir
+WORKDIR /home/work
+RUN echo 'source /opt/ros/$ROS_DISTRO/setup.bash' >> ~/.bashrc
+CMD ["bash"]
--- run.sh
+++ run.sh
@@ -1,4 +1,57 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IMAGE_NAME="my-ros2:dev"
+
+# Colors
+RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'; NC='\033[0m'
+
+LEVELS_UP=${LEVELS_UP:-1}
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+MOUNT_DIR="$SCRIPT_DIR"
+for _ in $(seq 1 "${LEVELS_UP}"); do
+  MOUNT_DIR="$(dirname "$MOUNT_DIR")"
+done
+
+echo -e "${YELLOW}Mounting host dir: ${MOUNT_DIR}${NC}"
+
+GPU_FLAG=""
+if command -v nvidia-smi &>/dev/null; then  
+  GPU_FLAG="--gpus all"
+  echo -e "${GREEN}GPU detected.${NC} Enabling GPU support for Docker."
+else
+  echo -e "${YELLOW}Warning:${NC} GPU not detected. Running without GPU support."
+fi
+
+
+
+# X11
+: "${DISPLAY:=${DISPLAY:-:0}}"
+XAUTH_HOST="${XAUTHORITY:-$HOME/.Xauthority}"
+if [[ ! -f "$XAUTH_HOST" ]]; then
+  echo -e "${YELLOW}Warn:${NC} $XAUTH_HOST 不存在"
+fi
+
+
+
+echo -e "${YELLOW}Running Docker container from image '$IMAGE_NAME'...${NC}"
 docker run -it --rm \
-  -v "$(pwd)/..":/workspace \
+  --net=host --ipc=host --privileged \
+  -e DISPLAY="$DISPLAY" \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY="$XAUTH_HOST" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v "$XAUTH_HOST":"$XAUTH_HOST":ro \
+  -v /dev:/dev \
+  -v "${MOUNT_DIR}":/workspace \
   -w /workspace \
-  my-ros2:dev
+  $GPU_FLAG \
+  "$IMAGE_NAME"
+
+rc=$?
+if [ $rc -eq 0 ]; then
+  echo -e "${GREEN}Success:${NC} Docker container exited successfully."
+else
+  echo -e "${RED}Error:${NC} Docker container failed to run. (exit $rc)"
+  exit $rc
+fi
